5f66d4d5eec1ee6a0d2ea14bb17007b5625bf10d
David Walter <hello@davidwalter.at>
2023-10-21 20:54:37 +0200

    Update WindowReader

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/divadretlaw/WindowReader", from: "1.1.0")
+        .package(url: "https://github.com/divadretlaw/WindowReader", from: "1.3.0")
     ],
     targets: [
         .target(name: "WindowSceneReader", dependencies: ["WindowReader"])
